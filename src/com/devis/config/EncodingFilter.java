0d74148261b58706a5fc9acba61d49ebc5bad2c8
yanomami <yanomami92@gmail.com>
2019-02-02 16:05:09 +0100

    add CharacterSetFilter for UTF8

--- src/com/devis/config/EncodingFilter.java
+++ src/com/devis/config/EncodingFilter.java
@@ -1,7 +1,5 @@
 package com.devis.config;
 
-import org.apache.catalina.filters.SetCharacterEncodingFilter;
-
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import java.io.IOException;
@@ -10,8 +8,9 @@ import java.io.IOException;
   * Servlet Filter implementation class EncodingFilter
   */
 @WebFilter("/EncodingFilter")
-public class EncodingFilter extends SetCharacterEncodingFilter {
+public class EncodingFilter implements Filter {
 
+    @Override
     public void doFilter( ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
         request.setCharacterEncoding("UTF-8");
